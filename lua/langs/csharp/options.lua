48f8de72a83c26eba46f3fc4cffa828c1305b0a5
Nguyen Quang Huy <quanghuy19062002@gmail.com>
2026-08-26 14:58:40 +0700

    chore(C#): Tuỳ chỉnh lại các màu tuỳ chỉnh phù hợp với bảng màu Kanagawa

--- lua/langs/csharp/options.lua
+++ lua/langs/csharp/options.lua
@@ -17,10 +17,12 @@ vim.api.nvim_create_autocmd("FileType", {
 -- ---- Khối 2: Highlight màu riêng cho C# ----
 -- Điều chỉnh màu semantic token để phân biệt rõ
 -- class / method / member thay vì màu mặc định.
-vim.api.nvim_set_hl(0, "@lsp.type.field.cs", { fg = "#63bdb5" })
-vim.api.nvim_set_hl(0, "@csharp.variable", { fg = "#61AFEF" })
-vim.api.nvim_set_hl(0, "@csharp.member", { fg = "#E5C07B" })
-vim.api.nvim_set_hl(0, "@csharp.method", { fg = "#C678DD" })
-vim.api.nvim_set_hl(0, "@csharp.class", { fg = "#4c9b8a" })
+vim.api.nvim_set_hl(0, "@lsp.type.field.cs",          { fg = "#7AA89F" })
+vim.api.nvim_set_hl(0, "@csharp.variable",            { fg = "#C0A36E" })
+vim.api.nvim_set_hl(0, "@csharp.member",              { fg = "#98BB6C" })
+vim.api.nvim_set_hl(0, "@csharp.method",              { fg = "#7E9CD8" })
+vim.api.nvim_set_hl(0, "@csharp.class",               { fg = "#E6C384" })
+vim.api.nvim_set_hl(0, "@lsp.type.controlKeyword.cs", { fg = "#D27E99" })
+vim.api.nvim_set_hl(0, "@lsp.type.keyword.cs",        { fg = "#957FB8" })
 vim.api.nvim_set_hl(0, "@lsp.type.controlKeyword.cs", { fg = "#c688c0" })
 vim.api.nvim_set_hl(0, "@lsp.type.keyword.cs", { fg = "#5b9bd5" })
