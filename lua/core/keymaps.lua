435832618a645cb40cd181eab8580c1c253621ca
Nguyen Quang Huy <quanghuy19062002@gmail.com>
2026-09-04 13:54:41 +0700

    feat(main): Luôn mở tab mới khi gd/gi

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,9 +3,38 @@ local telescope = require('telescope.builtin')
 
 vim.g.mapleader = " "
 
-map("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to definition" })
+local function lsp_jump_to_tab(method)
+  local params = vim.lsp.util.make_position_params(0, "utf-16")
+
+  vim.lsp.buf_request(0, "textDocument/" .. method, params, function(err, result)
+    if err or not result or vim.tbl_isempty(result) then
+      vim.notify("Không tìm thấy", vim.log.levels.INFO)
+      return
+    end
+
+    local locations = vim.islist(result) and result or { result }
+
+    -- Tạo tab mới
+    vim.cmd("tab split")
+
+    -- Nhảy tới kết quả đầu tiên
+    vim.lsp.util.jump_to_location(
+      locations[1],
+      "utf-16",
+      true
+    )
+  end)
+end
+
+map("n", "gd", function()
+  lsp_jump_to_tab("definition")
+end, { desc = "LSP: Go to definition (tab)" })
+
+map("n", "gi", function()
+  lsp_jump_to_tab("implementation")
+end, { desc = "LSP: Go to implementation (tab)" })
+
 map("n", "gr", vim.lsp.buf.references, { desc = "LSP: References" })
-map("n", "gi", vim.lsp.buf.implementation, { desc = "LSP: Go to implementation" })
 map("n", "K", function()
   vim.lsp.buf.hover({
     border = "rounded",
