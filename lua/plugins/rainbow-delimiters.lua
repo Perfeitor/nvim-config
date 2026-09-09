82db344e28c2718d612d7dfbd6861350cf02a76b
Nguyen Quang Huy <quanghuy19062002@gmail.com>
2026-09-09 10:25:00 +0700

    fix(main): Sửa lỗi ngoặc đơn sắc do thiếu khung treesitter dùng chung
    - Thêm lua/plugins/treesitter.lua lót đường, không liệt kê parser ở đây
    - Mỗi ngôn ngữ tự quản parser riêng trong lua/langs/<tên>/treesitter.lua
    - Giữ màu đỏ tùy chỉnh #eba0ac cho RainbowDelimiterRed
    - Thêm ColorScheme autocmd để kanagawa-wave không ghi đè màu
    - Bật treesitter riêng cho Lua (parser có sẵn trong runtime)

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,3 +1,9 @@
 require("rainbow-delimiters.setup").setup({})
 
 vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#eba0ac" })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#eba0ac" })
+  end,
+})
